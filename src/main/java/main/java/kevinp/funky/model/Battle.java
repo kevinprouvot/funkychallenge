08a183703cf3c6770768d6ddae13c278e7ac6dc5
KnProuvot <kroarr@mail.com>
2016-10-12 21:28:52 +0100

    handled adding hero to Battle

--- src/main/java/main/java/kevinp/funky/model/Battle.java
+++ src/main/java/main/java/kevinp/funky/model/Battle.java
@@ -10,17 +10,33 @@ public class Battle {
 	private long startTime;
 	private long endTime;
 	private List<Hero> heroes;
+	private Grid grid;
 	
-	public Battle(Integer id) {
+	private static final Integer gridMaxX = 100;
+	private static final Integer gridMaxY = 100;
+	private static final Integer maxHero = 2;
+	
+	public Battle(Integer id, Hero hero) {
 		status = EBattleStatus.WAITING;
 		startTime = 0;
 		endTime = 0;
 		heroes = new ArrayList<>();
+		heroes.add(hero);
+	}
+	
+	public void addHero(Hero hero) {
+		if (EBattleStatus.WAITING.equals(status)) {
+			heroes.add(hero);
+			if (heroes.size() == maxHero) {
+				status = EBattleStatus.ACTIVE;
+			}
+		}
 	}
 	
 	public void startBattle() {
-		status = EBattleStatus.ACTIVE;
 		startTime = Calendar.getInstance().getTimeInMillis();
+		grid = new Grid(gridMaxX, gridMaxY);
+		status = EBattleStatus.ACTIVE;
 	}
 	
 	/**
@@ -51,5 +67,9 @@ public class Battle {
 	 */
 	public long getBattleDuration() {
 		return endTime - startTime;
+	}
+	
+	public Grid getGrid() {
+		return this.grid;
 	}
 }
